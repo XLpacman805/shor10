9ecd8d7c1942c42fc30e998c10f3ad045ffb3143
System <none>
2017-08-16 01:50:24 +0000

    Shor10 url shortening service is fully function. UI is next.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const mongo = require('mongodb').MongoClient;
 const MONGODB_URI = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.DB_PORT+'/'+process.env.DB;
 const randomString = require("random-string");
-var homeUrl = "https://shor10.glitch.me/";
+var homeUrl = "https://shor10.glitch.me/r/";
 
 
 // http://expressjs.com/en/starter/static-files.html
@@ -31,46 +31,59 @@ function redirector(urlName, response){//url name is the last 4 characters in sh
   
 }//end redirector function
 
-function generateOriginal(new_url, response){
-  //Connect to db and serch for new_url
+function generateUniqueName(dbc, response, db, full_url){ //dbc === data base collection
+  var new_name = randomString({length:4}).toLowerCase();
+  //check if new name exists in db.
+   dbc.count({name: new_name}, (err, count)=>{
+          if(err) throw err; 
+      
+          if(count > 0){//new_name exists ,generate a new string
+            console.log("defined");
+            generateUniqueName(dbc, response, db);
+          }else{//new_name doesn't exist. 
+            console.log("new_name is available, good to go.");
+            //create a new db object for new_name and insert it into the db
+            dbc.insert(
+              {
+                name:new_name,
+                full_url: full_url,
+                short_url: homeUrl + new_name
+                
+              }, (err, element)=>{
+                if(err) throw err; 
+                db.close(response.end(JSON.stringify({
+                  name: new_name,
+                  full_url: full_url,
+                  short_url: homeUrl + new_name
+                })));
+            });    
+          }            
+        });
   
+}//end uniqueString()
+
+function shorten(new_url, response){
+  //Connect to db and serch for new_url  
   mongo.connect(MONGODB_URI, (err, db) =>{
     if(err) throw err; 
     var links = db.collection("links");
     
     links.find({
       full_url: new_url
-    }).toArray((err, docs)=>{
+    },{_id:0}).toArray((err, docs)=>{
       if(err) throw err; 
       console.log("still working");
       var result = JSON.stringify(docs[0]); 
       //check if result is defined
       if(!Boolean(result)){ //new_url not defined in db
-        var new_name = randomString({length:4}).toLowerCase();
-        //check if new name exists in db. 
-        
-        links.count({name: new_name}, (err, count)=>{
-          if(err) throw err; 
-          if(count > 0){//new_name exists
-            db.close(response.end("defined"));
-          }else{//new_name doesn't exist. 
-            db.close(response.end("not defined"));
-          }
-            
-        });
-        
+        generateUniqueName(links, response, db, new_url);
       }else{//new_url already exists in the db, just send the user the existing object. 
         db.close(response.end(result));
       }
     });
     
   });
-  
-  
-  
 }//end generateOriginal()
-
-
 
 app.get("/r/:name", (req, res)=>{
   var name = req.params.name;
@@ -79,9 +92,12 @@ app.get("/r/:name", (req, res)=>{
 
 app.get("/new/",(req,res) =>{
   var new_url = req.query.url;
-  generateOriginal(new_url, res);
+  if(new_url.includes("http://") || new_url.includes("https://")){
+    shorten(new_url, res);
+  }else{
+    res.end(JSON.stringify({name:"error",full_url:"error",short_url:"error", error:"You must include http:// or https:// in your url"}));
+  }
 });
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
