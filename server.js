96ff22ca9808301115e2d30020d6d6578fd5d48f
System <none>
2017-08-16 00:41:44 +0000

    making progess with the generateOriginal function

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mongo = require('mongodb').MongoClient;
 const MONGODB_URI = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.DB_PORT+'/'+process.env.DB;
+const randomString = require("random-string");
+var homeUrl = "https://shor10.glitch.me/";
 
 
 // http://expressjs.com/en/starter/static-files.html
@@ -9,7 +11,6 @@ app.use(express.static('views'));
 
 function redirector(urlName, response){//url name is the last 4 characters in short-url. mydomain.com/a3fs => a3fs
   //search for urlName in in db.
-  var homeUrl = "https://shor10.glitch.me/";
   urlName.toLowerCase();
   console.log("urlName: " + urlName);
   
@@ -23,16 +24,62 @@ function redirector(urlName, response){//url name is the last 4 characters in sh
     }).toArray( (err, docs) =>{
       if(err) throw err; 
       console.log("found "+urlName);
-      var fullUrl = docs[0]["full-url"];
+      var fullUrl = docs[0]["full_url"];
       db.close(response.redirect(fullUrl));
     });
   });
   
 }//end redirector function
 
+function generateOriginal(new_url, response){
+  //Connect to db and serch for new_url
+  
+  mongo.connect(MONGODB_URI, (err, db) =>{
+    if(err) throw err; 
+    var links = db.collection("links");
+    
+    links.find({
+      full_url: new_url
+    }).toArray((err, docs)=>{
+      if(err) throw err; 
+      console.log("still working");
+      var result = JSON.stringify(docs[0]); 
+      //check if result is defined
+      if(!Boolean(result)){ //new_url not defined in db
+        var new_name = randomString({length:4}).toLowerCase();
+        //check if new name exists in db. 
+        
+        links.count({name: new_name}, (err, count)=>{
+          if(err) throw err; 
+          if(count > 0){//new_name exists
+            db.close(response.end("defined"));
+          }else{//new_name doesn't exist. 
+            db.close(response.end("not defined"));
+          }
+            
+        });
+        
+      }else{//new_url already exists in the db, just send the user the existing object. 
+        db.close(response.end(result));
+      }
+    });
+    
+  });
+  
+  
+  
+}//end generateOriginal()
+
+
+
 app.get("/r/:name", (req, res)=>{
   var name = req.params.name;
   redirector(name,res);
+});
+
+app.get("/new/",(req,res) =>{
+  var new_url = req.query.url;
+  generateOriginal(new_url, res);
 });
 
 
