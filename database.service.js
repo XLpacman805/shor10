fe6cb2141da36ad0394806907bc7c983138a61b6
Johnny Meza <webmanager@mxssolutions.com>
2020-01-21 18:56:07 -0800

    Updated architecture to not check for existing url

--- database.service.js
+++ database.service.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mongo = require('mongodb').MongoClient;
 const MONGODB_URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds062059.mlab.com:62059/url-shortener-development`;
+const nanoid = require('nanoid');
+
 
 /**
  * Finds the short URL in the database by the uniqueId.
@@ -19,37 +21,18 @@ function getShortUrl (uniqueId) {
 }
 
 /**
- * Hashes a unique id based on url string. 
- * @param {URL.href} urlString
- * @returns {String} 
- */
-function createUniqueId(urlString) {
-    //return a hashed url string.
-    return String; 
-}
-
-function setShortUrl (uniqueId) {
-    return 1; 
-}
-
-/**
- * Creates a short url and returns one if it already exists. 
+ * Creates a short url and returns one it. 
  * @param {URL.href} urlString - The full valid URL string. 
  * @returns {Promise}
  */
 exports.createShortUrl = function (urlString) {
     return new Promise((resolve, reject) => {
-        //hash url.
+        let uniqueId = nanoid(7); 
 
-        //Check if url is already shortened by:
-        //find short url. 
-        //if short url found, resolve that. 
-
-        // if short url not found: 
-        //
+        // set the short url. 
 
         if (true) {
-            resolve(urlString); //Resolve parameter just to test data flow. Remove this later.
+            resolve(uniqueId); //Resolve parameter just to test data flow. Remove this later.
         } else {
             reject("Failure in database.service.createShortUrl");
         }
